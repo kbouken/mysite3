5ec721baade269a868ec6d9b721414ecbf74cc1b
kbouken <61100187+kbouken@users.noreply.github.com>
2021-10-16 09:53:55 +0900

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,12 @@
+// idがweatherとついた要素を取得し、elementに代入
+// var element = document.getElementById("weather");
+// 代入されたelement を innerHTML を使用して書き換える
+// element.innerHTML = "<span>曇り</span>";
+
+    var element = document.querySelector(".weather");
+    element.style.cssText = "color:#ff7c89; text-decoration:underline;";
+
+// element.className = "sample";
+
+// var element = document.querySelector("dl dt");
+//  console.log(element);
